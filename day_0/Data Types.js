b89ba83b47a1d8d2135ed810b5cb6ec466541204
phannguyen020501 <57362109+phannguyen020501@users.noreply.github.com>
2020-04-04 19:47:26 +0700

    commit day_0

---
+++ day_0/Data Types.js
@@ -0,0 +1,12 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+    // Declare a variable named 'firstInteger' and initialize with integer value 4.
+    const firstInteger = 4;
+    
+    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+    const firstDecimal = 4.0;
+    
+    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+    const firstString = 'HackerRank ';
+    console.log(parseInt(firstInteger)+parseInt(secondInteger));
+    console.log(parseFloat(firstDecimal)+parseFloat(secondDecimal));
+    console.log(firstString+secondString);
